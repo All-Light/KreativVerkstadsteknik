9115daa8ba435422c65a1964bde3b74c5b54c73e
All-Light <alexander1868@hotmail.com>
2025-02-02 10:29:52 +0100

    changed pin config

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from CameraController import Camera
 import time
 
 if __name__ == "__main__":
-    overwatch = Overwatch(yaw_pin=18, pitch_pin=19, camera_index=0, displayFeed=True, boundingBox=0.3)
+    overwatch = Overwatch(yaw_pin=19, pitch_pin=18, camera_index=0, displayFeed=False, boundingBox=0.3)
     overwatch.start()
